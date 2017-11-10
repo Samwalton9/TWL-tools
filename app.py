11fa309ed266bdddb72bacd7fbff1809779eb085
Samwalton9 <swalton@wikimedia.org>
2017-11-10 14:17:32 +0000

    Added simple site login

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # A good portion of this code is based on the Flask tutorial (http://flask.pocoo.org/docs/0.12/tutorial/)
+# and flask-login tutorial (https://github.com/maxcountryman/flask-login)
 #
 # Copyright (c) 2017 Sam Walton and contributors
 #
@@ -21,9 +22,9 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-import os
-import sqlite3
 import flask
+import flask_login
+import os
 import gspread
 import numpy as np
 from oauth2client.service_account import ServiceAccountCredentials
@@ -33,19 +34,48 @@ from fnmatch import fnmatch
 app = flask.Flask(__name__)
 app.config.from_object(__name__)
 
-app.config.update(dict(
-	USERNAME='admin',
-	PASSWORD='default' #TODO: Move this out to a secret username & password
-))
 app.config.from_envvar('TWLTOOLS_SETTINGS', silent=True)
 
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
 __dir__ = os.path.dirname(__file__)
+
+loaded_password = open(os.path.join(__dir__,'site_password')).read()[:-1]
+
+users = {'admin':{'password': loaded_password}}
+
+app.secret_key = open(os.path.join(__dir__,'secret_key')).read()[:-1]
+
+class User(flask_login.UserMixin):
+	pass
+
+@login_manager.user_loader
+def load_user(username):
+	if username not in users:
+		return None
+	
+	user = User()
+	user.id = username
+	return user
+
+@login_manager.request_loader
+def request_loader(request):
+	username = request.form.get('username')
+	if username not in users:
+		return None
+
+	user = User()
+	user.id = username
+
+	return user
 
 @app.route('/')
 def index():
 	return flask.render_template('index.html')
 
 @app.route('/periodicals', methods=['GET', 'POST'])
+@flask_login.login_required
 def periodicals():
 
 	if flask.request.method == 'POST':
@@ -59,6 +89,7 @@ def periodicals():
 	return flask.render_template('periodicals.html', results=results)
 
 @app.route('/pageviews')
+@flask_login.login_required
 def pageviews():
 
 	logs_list = list_logs(os.path.join(__dir__,"logs/"),"*.txt")
@@ -73,30 +104,39 @@ def pageviews():
 
 
 @app.route('/pageviews/<log_file>')
+@flask_login.login_required
 def individual_log(log_file):
 	results = open('logs/%s_pageviews_log.txt' % log_file, 'r').readlines()
 
 	return flask.render_template('pageviews.html', results=results, type='log')
 
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-# 	error = None
-# 	if flask.request.method == 'POST':
-# 		if flask.request.form['username'] != app.config['USERNAME']:
-# 			error = 'Invalid username'
-# 		elif flask.request.form['password'] != app.config['PASSWORD']:
-# 			error = 'Invalid password'
-# 		else:
-# 			flask.session['logged in'] = True
-# 			flask.flash('Logged in successfully')
-# 			return flask.redirect(flask.url_for('index'))
-# 	return flask.render_template('login.html', error=error)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
 
-# @app.route('/logout')
-# def logout():
-# 	flask.session.pop('logged_in', None)
-# 	flask.flash('Logged out')
-# 	return flask.redirect(flask.url_for('index'))
+	if flask.request.method == 'POST':
+		username = flask.request.form['username']
+		if username in users:
+			if flask.request.form['password'] == users[username]['password']:
+				user = User()
+				user.id = username
+				flask_login.login_user(user)
+				flask.flash('Logged in successfully')
+				return flask.redirect(flask.url_for('index'))
+			else:
+				error_msg = 'Incorrect password'
+				return flask.render_template('login.html', error=error_msg)
+		else:
+			error_msg = 'Incorrect username'
+			return flask.render_template('login.html', error=error_msg)
+	
+	return flask.render_template('login.html')
+
+@app.route('/logout')
+@flask_login.login_required
+def logout():
+	flask_login.logout_user()
+	flask.flash('Logged out successfully')
+	return flask.redirect(flask.url_for('index'))
 
 def list_logs(log_directory, file_pattern):
 	final_list = []
