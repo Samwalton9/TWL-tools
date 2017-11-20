5c25a958f6df8bc314ad525071ec771fdd915d26
Samwalton9 <swalton@wikimedia.org>
2017-11-20 13:36:59 +0000

    Go back to oursql if I can get it installed

--- UpdateMetrics.py
+++ UpdateMetrics.py
@@ -1,7 +1,6 @@
 import gspread
 import time
-import sqlite3
-import os
+import oursql
 import mwclient
 import numpy as np
 import logins
@@ -65,7 +64,12 @@ for i, search_term in enumerate(url_list):
     else:
       url_split_reversed = url_split_dots[::-1]
 
-    db = sqlite3.connect(database = '%swiki-p.rrdb.toolserver.org' % url_language)
+    db = oursql.connect(db = '%swiki-p' % url_language,
+                        host = '%swiki-p.rrdb.toolserver.org' % url_language,
+                        read_default_file = os.path.expanduser("~/replica.my.cnf"),
+                        charset=None,
+                        use_unicode=False
+                        )
     cursor = db.cursor()
     # site = mwclient.Site(('https', '%s.wikipedia.org' %language_list[i].strip()), clients_useragent=ua)
 
