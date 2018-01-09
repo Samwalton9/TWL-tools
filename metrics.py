21bd2a6266ee78c88f2f3d827b156c67bf5e4be2
Samwalton9 <swalton@wikimedia.org>
2018-01-09 12:20:21 +0000

    Added support for TWL staff notes for each URL

--- metrics.py
+++ metrics.py
@@ -19,11 +19,12 @@ class CollectMetrics:
 		self.URL_names = self.metrics_data['URL'].tolist()
 		self.URL_domains = self.metrics_data['Domain'].tolist()
 		self.display_check = self.metrics_data['Display?'].tolist()[0:num_partners+1]
+		self.notes = self.metrics_data['Notes'].tolist()
 
 		languages = self.metrics_data['Language'].tolist()
 		self.language_list = [pycountry.languages.get(alpha_2=i) for i in languages]
 
-		self.metrics_dates = self.metrics_data.columns.tolist()[6:]
+		self.metrics_dates = self.metrics_data.columns.tolist()[7:]
 
 	def list_partners(self):
 		partner_list = [x for i,x in enumerate(self.partner_names) if self.display_check[i] == 'x']
@@ -33,7 +34,7 @@ class CollectMetrics:
 		selected_urls = []
 		for i, URL_domain in enumerate(self.URL_domains):
 			if self.partner_names[i] == self.partner_name and self.display_check[i] == 'x':
-				this_partner_metrics = self.metrics_data.iloc[i,6:].tolist()
+				this_partner_metrics = self.metrics_data.iloc[i,7:].tolist()
 				this_partner_dates = [self.metrics_dates[i]
 									  for i, x in enumerate(this_partner_metrics)
 									  if pd.notna(x)]
@@ -54,11 +55,14 @@ class CollectMetrics:
 					chart_start = min_links * 0.9
 
 				if max_links > 15:
-					chart_height = round(max_links + ((max_links - min_links) * 0.1),-1)
+					chart_height = round(max_links + ((max_links - min_links) * 0.1))
 				else:
 					chart_height = 15
 
 				dates_iso = ["%s-%s-%s" %(date.split("/")[2], date.split("/")[0], date.split("/")[1]) for date in this_partner_dates]
+				this_partner_note = None
+				if pd.notna(self.notes[i]):
+					this_partner_note = self.notes[i]
 
 				selected_urls.append({'URL name': self.URL_names[i],
 									  'Language': self.language_list[i].name,
@@ -66,7 +70,8 @@ class CollectMetrics:
 									  'Link numbers': this_partner_metrics,
 									  'Link dates': dates_iso,
 									  'chart_height': chart_height,
-									  'chart_start': round(chart_start,-1)
+									  'chart_start': round(chart_start,-1),
+									  'notes': this_partner_note
 									 })
 		if len(selected_urls) > 0:
 			return selected_urls
