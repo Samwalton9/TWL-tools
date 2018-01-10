1161f2afdd900effefab478817db5f7fa4447eac
Samwalton9 <swalton@wikimedia.org>
2018-01-10 15:23:05 +0000

    Bug fix

--- download_metrics.py
+++ download_metrics.py
@@ -5,7 +5,7 @@ def download_csv():
 	metrics_sheet = g_client.open_by_key('1W7LRnkBrppOx_Yhoa8r0XBMDowGCjmmaHEol7Cj1TvM')
 	first_metrics_worksheet = metrics_sheet.get_worksheet(0)
 
-	csv_export = first_worksheet.export()
+	csv_export = first_metrics_worksheet.export()
 	with open('metrics.csv', 'wb') as f:
 		f.write(csv_export)
 
